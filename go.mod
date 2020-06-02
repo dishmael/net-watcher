a40f30299698be242a33ec052d798ef5bc640175
dishmael <dishmael3@gmail.com>
2020-06-02 08:33:20 -0400

    added ping logic

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dishmael/net-watcher
+
+go 1.14
+
+require (
+	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,101 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/tatsushid/go-fastping"
+)
+
+// Statistics ...
+type Statistics struct {
+	Count int       `json:"count"`
+	Min   float64   `json:"min"`
+	Max   float64   `json:"max"`
+	Avg   float64   `json:"avg"`
+	Value float64   `json:"value"`
+	Start time.Time `json:"-"`
+}
+
+var min, max, avg float64 = 0, 0, 0
+
 func main() {
+	var endpoint string
+
+	stats := &Statistics{
+		Count: 0,
+		Max:   0,
+		Min:   0,
+		Avg:   0,
+		Value: 0,
+		Start: time.Now(),
+	}
+
+	go handleSigTerm(stats)
+
+	if len(os.Args) < 2 {
+		endpoint = "www.google.com"
+	} else {
+		endpoint = os.Args[1]
+	}
+
+	addy, err := net.ResolveIPAddr("ip4:icmp", endpoint)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	for {
+		ping(stats, addy)
+	}
+}
+
+func ping(stats *Statistics, ip *net.IPAddr) {
+	p := fastping.NewPinger()
+	p.AddIPAddr(ip)
+
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		stats.Value = rtt.Seconds() * 1000
+
+		// Calc Min
+		if stats.Value < stats.Min || stats.Min == 0 {
+			stats.Min = stats.Value
+		}
+
+		// Calc Max
+		if stats.Value > stats.Max {
+			stats.Max = stats.Value
+		}
+
+		// Calc Avg
+		stats.Avg = (stats.Avg + stats.Value) / 2
+
+		fmt.Printf("IP Addr: %s receive, RTT: %.3f \n", addr.String(), stats.Value)
+	}
+
+	stats.Count++
+	p.Run()
+}
+
+func handleSigTerm(stats *Statistics) {
+	// Enable the capture of Ctrl-C, to cleanly close the application
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	_ = <-c
+
+	elapsed := time.Since(stats.Start)
+
+	fmt.Printf("\r")
+	fmt.Printf("\r----\nStatistics: elapsed=%s count=%d min=%.3f max=%.3f avg=%.3f\n\n",
+		elapsed,
+		stats.Count,
+		stats.Min,
+		stats.Max,
+		stats.Avg,
+	)
+
+	os.Exit(0)
 }
